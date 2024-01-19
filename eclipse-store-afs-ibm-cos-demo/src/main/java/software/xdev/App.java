c77cf018515c7686e00b40de3748091ff9fc6e75
AB <a.bierler@xdev-software.de>
2024-01-19 11:18:22 +0100

    Fix checkstyle

--- eclipse-store-afs-ibm-cos-demo/src/main/java/software/xdev/App.java
+++ eclipse-store-afs-ibm-cos-demo/src/main/java/software/xdev/App.java
@@ -20,11 +20,13 @@ import com.ibm.cloud.objectstorage.services.s3.AmazonS3ClientBuilder;
 import software.xdev.eclipse.store.afs.ibm.cos.types.CosConnector;
 
 
-public class App
+@SuppressWarnings("checkstyle:MagicNumber")
+public final class App
 {
-	private static final String COS_ENDPOINT = "";// eg "https://s3.us.cloud-object-storage.appdomain.cloud"
+	private static final String COS_ENDPOINT = ""; // eg "https://s3.us.cloud-object-storage.appdomain.cloud"
 	private static final String COS_API_KEY_ID = ""; // eg "0viPHOY7LbLNa9eLftrtHPpTjoGv6hbLD1QalRXikliJ"
-	private static final String COS_SERVICE_CRN = ""; // "crn:v1:bluemix:public:cloud-object-storage:global:a/<CREDENTIAL_ID_AS_GENERATED>:<SERVICE_ID_AS_GENERATED>::"
+	private static final String COS_SERVICE_CRN = "";
+	// "crn:v1:bluemix:public:cloud-object-storage:global:a/<CREDENTIAL_ID_AS_GENERATED>:<SERVICE_ID_AS_GENERATED>::"
 	private static final String COS_BUCKET_LOCATION = ""; // eg "us"
 	private static final String BUCKET_NAME = "";
 	
@@ -63,20 +65,24 @@ public class App
 	}
 	
 	public static AmazonS3 createClient(
-		final String api_key,
-		final String service_instance_id,
-		final String endpoint_url,
+		final String apiKey,
+		final String serviceInstanceId,
+		final String endpointUrl,
 		final String location)
 	{
-		final AWSCredentials credentials = new BasicIBMOAuthCredentials(api_key, service_instance_id);
+		final AWSCredentials credentials = new BasicIBMOAuthCredentials(apiKey, serviceInstanceId);
 		final ClientConfiguration clientConfig = new ClientConfiguration().withRequestTimeout(-1);
 		clientConfig.setUseTcpKeepAlive(true);
 		
 		return AmazonS3ClientBuilder.standard()
 			.withCredentials(new AWSStaticCredentialsProvider(credentials))
-			.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint_url, location))
+			.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpointUrl, location))
 			.withPathStyleAccessEnabled(true)
 			.withClientConfiguration(clientConfig)
 			.build();
+	}
+	
+	private App()
+	{
 	}
 }
